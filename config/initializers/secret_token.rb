cf8f1e07f207d4d7762a685de109a9529c5c11fd
Nick Taberacle <nick@epcess.co.ukgit clone git@git.assembla.com:csdm-fdcd ..git config --global user.email nick@epcess.co.uk>
2013-01-07 17:06:23 +0000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hoo::Application.config.secret_token = '5e6dd0d62abb9a551adb4b449cde0c78291068d0912877dd3031c9aa085be464fe8d9ef06be84e147445ef7e7ff265275ca11406f812a058392a78745af4db71'
