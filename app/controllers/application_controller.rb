d1acc51020130f864825fbcf87dcc69afbc5e7c6
Nick Taberacle <nick@epcess.co.ukgit clone git@git.assembla.com:csdm-fdcd ..git config --global user.email nick@epcess.co.uk>
2013-01-09 01:43:13 +0000

    Before_filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :authenticate
 
 protected
 
 #Temp Security Attr
-  if RAILS_ENV= "production"
+  if RAILS_ENV= "development"
     def authenticate
       authenticate_or_request_with_http_basic do |username, password|
-      username == "foo" && password == "bar"
+      username == "admin" && password == "waterloo12"
                                               end
     end
  end
