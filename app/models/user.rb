06d687afcc35f9929e50cdf08bfab2ba0676ce7f
Nick Taberacle <nick@epcess.co.ukgit clone git@git.assembla.com:csdm-fdcd ..git config --global user.email nick@epcess.co.uk>
2013-01-15 09:41:23 +0000

    Facebook sandbox fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
       attr_accessible :email, :provider, :uid, :oauth_token, :oauth_expires_at, :name
   
   #User Validation      
-  validates_uniqueness_of :email
+ # validates_uniqueness_of :email
   
   def self.from_omniauth(auth)
   where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
