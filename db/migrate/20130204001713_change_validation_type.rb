1c9e5578fea2b7d240806cfd0bb598cb74a15c15
Nick Tabernacle <nick@beo.so>
2013-02-08 15:04:28 +0000

    Rm valid int

--- db/migrate/20130204001713_change_validation_type.rb
+++
@@ -1,6 +0,0 @@
-class ChangeValidationType < ActiveRecord::Migration
-  def change
-    change_column :credit_validations, :user_id, :integer
-    change_column :credit_validations, :credit_id, :integer 
-  end
-end
