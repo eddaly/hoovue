9c899f4aba4e59277d84a040174ad97b4b07a6a5
Nick Tabernacle <nick@beo.so>
2013-02-13 03:16:24 +0400

    Minor activity feed fixes

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -1,6 +1,6 @@
 class FrontController < ApplicationController
   def index
-    @credits = Credit.limit(10)
+    @credits = Credit.limit(10).order("created_at DESC")
      @products = Product.search(params[:search])
   end
 end
