e8de07cafc1cad841a98bebf846d66f754986c39
Nick Tabernacle <nick@beo.so>
2013-02-21 18:04:38 +0000

    Add product (work)

--- app/models/product_genre.rb
+++ app/models/product_genre.rb
@@ -1,5 +1,6 @@
 class ProductGenre < ActiveRecord::Base
   attr_accessible :name, :fields_attributes
     has_many :fields, class_name: "ProductField"
+      has_many :products
       accepts_nested_attributes_for :fields, allow_destroy: true
 end
