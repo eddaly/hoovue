a5dc21756b8474e9377c4d1c776b92f9aecd8968
Nick Tabernacle <nick@beo.so>
2013-02-28 17:37:18 +0000

    Product upload changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,9 +12,7 @@ class Product < ActiveRecord::Base
   
 def self.import(file)
   CSV.foreach(file.path, headers: true) do |row|
-    product = find_by_id(row["id"]) || new
-    product.attributes = row.to_hash.slice(*accessible_attributes)
-    product.save!
+    Product.create! row.to_hash
   end
 end
 
