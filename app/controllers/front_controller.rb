68f4c89142398aa6b91df64490a8159d4238d9d9
Nick Tabernacle <nick@beo.so>
2013-03-06 15:47:04 +0000

    Navi Changes

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -3,6 +3,5 @@ class FrontController < ApplicationController
     @credits = Credit.confirmed.where(:user_id.blank? ).limit(10).order("created_at DESC")
      @products = Product.search(params[:search])
       @products_featured = Product.limit(3).order("RANDOM()")
-    
   end
 end
