3f2f7bc390c1a63c427643f4eff59e84a4bec261
Nick Tabernacle <nick@beo.so>
2013-03-08 10:11:38 +0000

    Credit Info Count

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -44,4 +44,22 @@
     });
 });
 
+  $(document).ready(function() {
+maxCharacters = 160;
 
+$('#count').text(maxCharacters);
+
+$('textarea').bind('keyup keydown', function() {
+    var count = $('#count');
+    var characters = $(this).val().length;
+
+    if (characters > maxCharacters) {
+        count.addClass('over');
+    } else {
+        count.removeClass('over');
+    }
+
+    count.text(maxCharacters - characters);
+});
+
+});
