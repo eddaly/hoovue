424aad7b77bbca682ed58c48fb13c22b5a15387d
Nick Tabernacle <nick@beo.so>
2013-03-14 22:50:48 +0000

    Credits on product page

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -5,6 +5,7 @@ class FrontController < ApplicationController
      @users_featured = User.limit(4).order("RANDOM()")
      if current_user
       @credit_validations = CreditValidation.where(:validator_id => current_user.id).where(:status => "pending")
+      @unclaimed = Credit.where(:pending_user_email => current_user.email).where(:user_id => nil)
        @flagged_credits = CreditValidation.where(:user_id => current_user.id).where(:status => "pending").where(:credit_id => true)  
        @empty_credits = Credit.where(:user_id => current_user.id).limit(3)
        @credit = Credit.new
