c4ea381a2aa689c0dca3e5acbb2b6c1bdb6fad34
Nick Tabernacle <nick@beo.so>
2013-05-18 17:58:33 +0100

    Front page spelling & User credits counts set to 0 & Chart beat uid & Google analytics

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,7 @@ class User < ActiveRecord::Base
     user.oauth_token = auth.credentials.token
     user.oauth_expires_at = Time.at(auth.credentials.expires_at)
     user.role = "user"
+    user.credits_count = "0"
    if user.id 
     else
       UserMailer.signup_confirmation(user).deliver
