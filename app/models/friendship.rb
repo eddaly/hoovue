818724921f9790fdeb59084852fa3d8d4fcfbdf0
Nick Tabernacle <nick@beo.so>
2013-06-07 15:20:10 +0100

    Validate followers

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,5 +1,6 @@
 class Friendship < ActiveRecord::Base
   attr_accessible :friend_id, :user_id
+    validates :friend_id, :uniqueness => { :scope => :user_id, :message => "You are already following this user." }
     belongs_to :user
       belongs_to :friend, :class_name => "User"
 end
