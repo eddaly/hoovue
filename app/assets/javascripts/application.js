f18588e8e9871bc24f110c7a9c1a11e11bf1a250
Nick Tabernacle <nick@beo.so>
2013-06-07 17:14:50 +0100

    Revert "JS updates"
    This reverts commit 9c45384fd6400276a6ad3b9a51df8df74399be5b.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,15 +10,12 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require_tree .
-
 //= require jquery
-//= require jquery_ujs
 //= require select2
 //= require rails.validations
 //= require jquery.nested-fields
 //= require best_in_place
-
+//= require_tree .
 
     $(document).ready(function() { $("select").select2({ width: 'element' }); });
 
