8c1e89900de7bb387eb43d38cff7c8a3ea8769b8
Nick Tabernacle <nick@beo.so>
2013-08-16 20:23:02 +0100

    App settings for pipeline production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ Hoo::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = ['*.js', '*.css']
+  config.active_support.deprecation = :silence
 
   # Generate digests for assets URLs
   config.assets.digest = true
