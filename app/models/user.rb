c47e640cfa1a6116e567fca8c5e5a71b8b872777
Nick Tabernacle <nick@beo.so>
2013-09-05 14:31:18 +0100

    Bug fix - validate via facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,8 +20,6 @@ class User < ActiveRecord::Base
               
   #User Validation      
    # validates_uniqueness_of :email
-   
-  
   
   def self.from_omniauth(auth)
   where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
