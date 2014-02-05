a11beaf28cccd1b3f24dba4341579250954431b0
Nick Tabernacle <nick@beo.so>
2014-02-05 15:53:19 +0000

    Default thumb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -19,9 +19,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+   def default_url
+     "/assets/" + [version_name, "default-thumb.png"].compact.join('_')
+   end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
