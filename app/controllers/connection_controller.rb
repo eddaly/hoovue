a76501712368e659b5ee8e47b5ba7d3c98e70eb2
Nick Tabernacle <nick@beo.so>
2014-02-05 15:44:01 +0000

    Assets

--- app/controllers/connection_controller.rb
+++ app/controllers/connection_controller.rb
@@ -3,7 +3,7 @@ class ConnectionController < ApplicationController
   def index
     if @current_user
     @users = User.where(:name => @current_user.name).where("credits_count > ?", 0)
-  else
+    else
     redirect_to root_url
   end
   end
