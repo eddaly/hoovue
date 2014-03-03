1350f68379dd42509c85db797a1eb31bd88a35fb
Nick Tabernacle <nick@beo.so>
2014-03-03 10:00:31 +0000

    IP block range update

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -3,4 +3,8 @@ Rack::Attack.blacklist('block 5.10.83.61') do |req|
   '5.10.83.61' == req.ip
   '5.10.83.67' == req.ip
   '5.10.83.65' == req.ip
+  '5.10.83.26' == req.ip
+  '5.10.83.72' == req.ip
+  '5.10.83.85' == req.ip
+  
 end
