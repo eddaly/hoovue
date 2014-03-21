30553529d8248d4e53c0fa29fe6c9d314cbea333
Nick Tabernacle <nick@beo.so>
2014-03-21 17:44:38 +0000

    Front page re-design NOTE

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,7 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    if current_user.credits_count < 1
-    redirect_to connection_index_path(name: current_user.name), notice: "Thank you for signing up!"
-  else
-    redirect_to root_url, notice: "Welcome Back"
-  end
+    redirect_to root_url
   end
 
   def destroy
