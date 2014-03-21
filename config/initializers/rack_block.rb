17ec3ac576be93f480cc7ce1bc2b963ae913b2e3
Nick Tabernacle <nick@beo.so>
2014-03-21 00:34:38 +0000

    Block IP

--- config/initializers/rack_block.rb
+++ config/initializers/rack_block.rb
@@ -16,7 +16,14 @@ Hoo::Application.configure do
                    ip_pattern '208.115.113.' do
                        halt 401
                         end
+                        
+                        ip_pattern '192.187.115.' do
+                            halt 401
+                             end
+                             
+                             ip_pattern '199.233.' do
+                                 halt 401
+                                  end
   end
 
 end
-
