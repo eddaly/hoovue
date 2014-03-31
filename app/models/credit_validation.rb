4eff6ab2ec1aebe56db279fc79ee58ad1f3ff9a6
Nick Tabernacle <nick@beo.so>
2014-03-31 16:17:40 +0100

    Order

--- app/models/credit_validation.rb
+++ app/models/credit_validation.rb
@@ -1,7 +1,7 @@
 class CreditValidation < ActiveRecord::Base
   attr_accessible :credit_id, :user_id, :user_validation, :oneside, :token, :credit_validation_count, :token_created_at, :token_sent_at, :verified, :validator_id, :status, :current_credit_id  
         attr_accessor :current_credit_id  
-    belongs_to :credit, :counter_cache => :credit_validation_count
+          belongs_to :credit, :counter_cache => :credit_validation_count
            scope :confirmed, where(:status => "confirmed")
  
      validates_uniqueness_of :credit_id, :scope => :validator_id, :message => "This user has already validated you."
