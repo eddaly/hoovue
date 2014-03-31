10118a759eb9783a484dd3c2a4a2ee52fedf40b3
Nick Tabernacle <nick@beo.so>
2014-03-31 14:44:30 +0100

    C count

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -92,7 +92,6 @@ end
   def create 
     @credit = Credit.new(params[:credit])
     @credit.role = params[:role]
-    @credit.confirmed_validations_count = 0
     @credit.validations_count = 0
      @credit.product_id = @credit.product_idd
      if params[:batch]
