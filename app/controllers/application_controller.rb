c214424b190f062f4de985db9f3bdc57180653c1
Nick Tabernacle <nick@beo.so>
2014-03-31 20:35:37 +0100

    FB

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,25 +35,46 @@ class ApplicationController < ActionController::Base
     
     def update_facebook_params
       begin
+        
+        #Check if link params are true
+        
       if session[:pending_token] && if current_user
         @credit = Credit.find_by_pending_token(session[:pending_token])
-      @credit.user_id = current_user.id
-    
-        @credit.user_name = current_user.name
+        
+        #Update params 
+        
+          @credit.user_id = current_user.id
+          @credit.user_name = current_user.name
+          
+          #Find validators CV and update
+          
         @credit_validation = CreditValidation.find_by_id(@credit.credit_validation_id)
-        @credit_validation.validator_id = current_user.id
-        @credit_validation.status = "confirmed"
-        @credit_validation.save
-        @credit.confirmed_validations_count = @credit.credit_validations.confirmed.count
+          @credit_validation.validator_id = current_user.id
+          @credit_validation.status = "confirmed"
+          @credit.confirmed_validations_count = @credit.credit_validations.confirmed.count
+          @credit_validation.save
         if @credit.save
+          
+          #Create new CV for use and set as conf with id from validator
+          
          @credit_validation = CreditValidation.new(params[:credit_validation])
           @credit_validation.validator_id = @credit.validator_id
           @credit_validation.credit_id = @credit.id
           @credit_validation.status = "confirmed"
           @credit_validation.user_id = current_user.id
-          if @credit_validation.save
-          redirect_to root_url, :notice => "Your Facebook refered credit has been added."
-          session.delete(:pending_token)
+           @credit_validator = Credit.find_by_id(@credit_validation.credit_id)
+            if @credit_validation.save
+             
+              #Reset count on users C
+          
+                 
+                  @credit_validator.confirmed_validations_count = @credit_validator.credit_validations.confirmed.count
+                  @credit_validator.save
+                  
+                  #Finish and redirect
+               
+                redirect_to root_url, :notice => "Your Facebook refered credit has been added."
+                session.delete(:pending_token)
         end
           end
         end
@@ -66,43 +87,55 @@ class ApplicationController < ActionController::Base
     
     end
     
-        def update_credit_params
-          if session[:credit_id] && if current_user 
-          @credit = Credit.find(session[:credit_id])  
-            if @credit.pending_user_email == current_user.email
-               redirect_to @credit
-             @credit.user_id = current_user.id
-             @credit.user_name = current_user.name
-             @credit_validation = CreditValidation.find_by_id(@credit.credit_validation_id)
-             if @credit_validation
-             @credit_validation.status = "confirmed"
-             @credit_validation.validator_id = current_user.id
-             @credit_validation.update_attributes(params[:credit_validationn])
-             @credit_validator = Credit.find_by_id(@credit_validation.credit_id)
+def update_credit_params
+    if session[:credit_id] && if current_user 
+      
+      #Find linked credit and update prams
+      
+      @credit = Credit.find(session[:credit_id])  
+        if @credit.pending_user_email == current_user.email
+          redirect_to @credit
+          @credit.user_id = current_user.id
+          @credit.user_name = current_user.name
+          
+        #Find validators CV and update status  
+          
+          @credit_validation = CreditValidation.find_by_id(@credit.credit_validation_id)
+            if @credit_validation
+              @credit_validation.status = "confirmed"
+              @credit_validation.validator_id = current_user.id
+              @credit_validation.update_attributes(params[:credit_validationn])
+              
+              #Update validators C CV conf count
+              
+              @credit_validator = Credit.find_by_id(@credit_validation.credit_id)
               @credit_validator.confirmed_validations_count = @credit_validator.credit_validations.confirmed.count
-              @credit_validator.save
-           else
-           end
-             @credit_validation = CreditValidation.new(params[:credit_validation])
+                @credit_validator.save
+            else
+            end
+            
+            #Make new CV for new C and make status conf from validator
+            
+            @credit_validation = CreditValidation.new(params[:credit_validation])
                @credit_validation.user_id = current_user.id
                @credit_validation.validator_id = @credit.validator_id
                @credit_validation.status = "confirmed"
-               if  @credit.save
-                 @credit_validation.credit_id = @credit.id
-                 if  @credit_validation.save
-                   @credit.confirmed_validations_count = @credit.credit_validations.confirmed.count
-                   @credit.save
-                    session.delete(:credit_id)
-                  end
-                  end
+                 if  @credit.save
+                       @credit_validation.credit_id = @credit.id
+                       if  @credit_validation.save
+                         @credit.confirmed_validations_count = @credit.credit_validations.confirmed.count
+                         @credit.save
+                        session.delete(:credit_id) #Remove credit
+                      end
+                end
                   
                 else
                   redirect_to root_url, :notice => "The email that got you here isn't associated with your profile.  But you can add it <a href=/emails/new>here.</a>".html_safe
                     session.delete(:credit_id)     
-            end
-          end
-           end 
-         end
+                  end
+              end
+              end 
+end
          
          def current_user
            @current_user ||= User.find(session[:user_id]) if session[:user_id]
